544ad8db098cd7726c438aab3d12a9ad0219bc2a
Jaehyeon Han <jaehyeonhan99@gmail.com>
2026-05-21 00:47:02 +0900

    build: shortenUrl Lambda Terraform 명시

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,10 @@ provider "aws" {
   profile = var.aws_profile
 }
 
+data "aws_iam_role" "lambda_exec" {
+  name = var.lambda_role_name
+}
+
 resource "aws_dynamodb_table" "url_table" {
   name         = "UrlMapping"
   billing_mode = "PAY_PER_REQUEST"
@@ -26,5 +30,30 @@ resource "aws_dynamodb_table" "url_table" {
       key_type = "HASH"
     }
     projection_type = "ALL"
+  }
+}
+
+resource "aws_lambda_function" "shorten_url" {
+  function_name = "shortenUrl"
+
+  role    = data.aws_iam_role.lambda_exec.arn
+  handler = "index.handler"
+  runtime = "nodejs24.x"
+
+  filename         = "dummy.zip"
+  source_code_hash = filebase64sha256("dummy.zip")
+
+  environment {
+    variables = {
+      BASE_URL   = "https://jhan.cc"
+      TABLE_NAME = "UrlMapping"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      filename,
+      source_code_hash
+    ]
   }
 }
